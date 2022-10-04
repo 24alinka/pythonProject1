b4f54729879cadcfe18a34ae5c48b5fd82d2cd98
Alina <alinagritsan77@gmail.com>
2022-10-04 20:51:30 +0300

    last update 04/10/2022

--- pages/create_post_page.py
+++ pages/create_post_page.py
@@ -24,26 +24,31 @@ class CreatePostPage(BasePage):
         assert self.get_element_text(xpath=self.constants.SUCCESS_MESSAGE_XPATH) == self.constants.SUCCESS_MESSAGE_TEXT, \
             f"Actual: {self.get_element_text(xpath=self.constants.SUCCESS_MESSAGE_XPATH)}"
 
+    @log_decorator
     def delete_post(self):
         """Delete a post that was made"""
         self.click(xpath=self.constants.DELETE_POST_XPATH)
 
+    @log_decorator
     def verify_successfully_delete_post(self):
         """Verify the post is successfully deleted message"""
         assert self.get_element_text(
             xpath=self.constants.SUCCESS_DELETE_MESSAGE_XPATH) == self.constants.SUCCESS_DELETE_MESSAGE_TEXT, \
             f"Actual message: {self.get_element_text(xpath=self.constants.SUCCESS_DELETE_MESSAGE_XPATH)}"
 
+    @log_decorator
     def click_edit_post(self):
         """Edit post"""
         self.click(xpath=self.constants.EDIT_POST_BUTTON_XPATH)
 
+    @log_decorator
     def edit_post(self, title, body):
         """Update post using provided values"""
         self.fill_field(xpath=self.constants.TITLE_FIELD_XPATH, value=title)
         self.fill_field(xpath=self.constants.BODY_FIELD_XPATH, value=body)
         self.click(xpath=self.constants.UPDATE_POST_BUTTON_XPATH)
 
+    @log_decorator
     def verify_successfully_edit_post(self):
         """Verify that the body us updated and message appears"""
         assert self.get_element_text(xpath=self.constants.EDIT_MESSAGE_XPATH) == self.constants.EDIT_MESSAGE_TEXT, \
@@ -51,13 +56,15 @@ class CreatePostPage(BasePage):
         assert self.get_element_text(xpath=self.constants.BODY_FIELD_XPATH) == self.constants.UPDATE_POST_BODY_TEXT
         f"Actual message: {self.get_element_text(xpath=self.constants.BODY_FIELD_XPATH)}"
 
+    @log_decorator
     def create_post_with_click_checkbox(self, post):
-        """Create post with unique checkbox"""
+        """Create a unique post with checkbox"""
         self.fill_field(xpath=self.constants.TITLE_FIELD_XPATH, value=post.title)
         self.fill_field(xpath=self.constants.BODY_FIELD_XPATH, value=post.body)
         self.click(xpath=self.constants.CHECKBOX_XPATH)
         self.click(xpath=self.constants.CREATE_POST_BUTTON_XPATH)
 
+    @log_decorator
     def select(self, post, select_value):
         """Open select and choose a value"""
         self.fill_field(xpath=self.constants.TITLE_FIELD_XPATH, value=post.title)
@@ -65,6 +72,7 @@ class CreatePostPage(BasePage):
         self.select_field(xpath=self.constants.POST_SELECT_XPATH, value=select_value)
         self.click(xpath=self.constants.CREATE_POST_BUTTON_XPATH)
 
+    @log_decorator
     def verify_private_option(self):
         """Verify the post is private message"""
         assert self.get_element_text(
@@ -73,6 +81,7 @@ class CreatePostPage(BasePage):
         assert self.get_element_text(xpath=self.constants.SUCCESS_MESSAGE_XPATH) == self.constants.SUCCESS_MESSAGE_TEXT, \
             f"Actual: {self.get_element_text(xpath=self.constants.SUCCESS_MESSAGE_XPATH)}"
 
+    @log_decorator
     def verify_group_message_option(self):
         """Verify the post is private message"""
         assert self.get_element_text(
@@ -81,6 +90,7 @@ class CreatePostPage(BasePage):
         assert self.get_element_text(xpath=self.constants.SUCCESS_MESSAGE_XPATH) == self.constants.SUCCESS_MESSAGE_TEXT, \
             f"Actual: {self.get_element_text(xpath=self.constants.SUCCESS_MESSAGE_XPATH)}"
 
+    @log_decorator
     def verify_all_users_option(self):
         """Verify the post is private message"""
         assert self.get_element_text(
@@ -88,3 +98,26 @@ class CreatePostPage(BasePage):
             f"Actual: {self.get_element_text(xpath=self.constants.POST_SELECT_ALL_USERS_XPATH)}"
         assert self.get_element_text(xpath=self.constants.SUCCESS_MESSAGE_XPATH) == self.constants.SUCCESS_MESSAGE_TEXT, \
             f"Actual: {self.get_element_text(xpath=self.constants.SUCCESS_MESSAGE_XPATH)}"
+
+    @log_decorator
+    def create_post_with_all(self, post):
+        self.fill_field(xpath=self.constants.TITLE_FIELD_XPATH, value=post.title)
+        self.fill_field(xpath=self.constants.BODY_FIELD_XPATH, value=post.body)
+        self.select_field(xpath=self.constants.POST_SELECT_XPATH, value=post.select)
+        self.click(xpath=self.constants.CHECKBOX_XPATH)
+        self.click(xpath=self.constants.CREATE_POST_BUTTON_XPATH)
+
+    @log_decorator
+    def verify_create_full_post(self, title, body):
+        assert self.get_element_text(
+            xpath=self.constants.POST_SELECT_PRIVATE_XPATH) == self.constants.POST_SELECT_PRIVATE_TEXT, \
+            f"Actual: {self.get_element_text(xpath=self.constants.POST_SELECT_PRIVATE_XPATH)}"
+        assert self.get_element_text(xpath=self.constants.SUCCESS_MESSAGE_XPATH) == self.constants.SUCCESS_MESSAGE_TEXT, \
+            f"Actual: {self.get_element_text(xpath=self.constants.SUCCESS_MESSAGE_XPATH)}"
+        assert self.get_element_text(
+            xpath=self.constants.UNIQUE_MESSAGE_YES_XPATH) == self.constants.UNIQUE_MESSAGE_YES_TEXT, \
+            f"Actual: {self.get_element_text(xpath=self.constants.UNIQUE_MESSAGE_YES_XPATH)}"
+        assert self.get_element_text(xpath=self.constants.SAVED_POST_TITLE_XPATH) == title, \
+            f"Actual: {self.get_element_text(xpath=self.constants.SAVED_POST_TITLE_XPATH)}"
+        assert self.get_element_text(xpath=self.constants.SAVED_POST_BODY_XPATH) == body, \
+            f"Actual: {self.get_element_text(xpath=self.constants.SAVED_POST_BODY_XPATH)}"
